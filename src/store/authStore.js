93b4dc3ff75519eee9c51183da1ae69ac1fbbe39
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2025-01-11 01:32:12 +0530

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_BACKEND_API_URL || "http://localhost:5000/api/auth";
+  import.meta.env.VITE_BACKEND_API_URL
 
 axios.defaults.withCredentials = true;
 
