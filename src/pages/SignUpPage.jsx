a0ea274ee1d28958b9f2a4ab75a5bef198d6fb6a
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2025-01-11 01:10:39 +0530

    Update SignUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Loader, Lock, Mail, User } from "lucide-react";
-import Input from "../components/input";
+import Input from "../components/Input";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/passwordStrengthMeter";
